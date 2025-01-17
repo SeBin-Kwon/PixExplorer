bbbc9c1dae6a29a0203d89939cdbfa695a1736f9
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-17 18:17:17 +0900

    Chore: View와 ViewController 폴더 분리, Extension 추가

--- PixExplorer/Extension/UIViewController+Extension.swift
+++ PixExplorer/Extension/UIViewController+Extension.swift
@@ -5,4 +5,10 @@
 //  Created by Sebin Kwon on 1/17/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
