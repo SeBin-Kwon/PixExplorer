3b53f7a35e04d0e2c86b91f7ba833c6347e333ff
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-18 00:51:46 +0900

    Feat: UITableViewDataSourcePrefetching를 이용한 페이지네이션 구현

--- PixExplorer/ViewControllers/SearchViewController.swift
+++ PixExplorer/ViewControllers/SearchViewController.swift
@@ -12,6 +12,8 @@ class SearchViewController: BaseViewController {
     
     private let searchView = SearchView()
     var list = [Photo]()
+    var page = 1
+    var isEnd = false
     
     override func loadView() {
         view = searchView
@@ -21,21 +23,49 @@ class SearchViewController: BaseViewController {
         navigationItem.title = "SEARCH PHOTO"
         searchView.collectionView.delegate = self
         searchView.collectionView.dataSource = self
+        searchView.collectionView.prefetchDataSource = self
         searchView.collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.identifier)
-        callRequest()
+        callRequest(page: page)
     }
     
-    func callRequest() {
-        NetworkManager.shard.callRequest("cat", 1) { value in
-            print(#function)
+    func callRequest(page: Int) {
+        NetworkManager.shard.callRequest("cat", page) { value in
+            print(#function, "page:", page)
             guard let value else { return }
-            self.list = value.results
+            if page == 1 {
+                self.list = value.results
+            } else {
+                self.list.append(contentsOf: value.results)
+            }
+            if value.total == page {
+                self.isEnd = true
+            }
             self.searchView.collectionView.reloadData()
+            if page == 1 && !self.list.isEmpty {
+                self.searchView.collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
+            }
             print("전체 검색 수", value.total)
             print("총 페이지 수", value.total_pages)
         }
     }
+}
+
+extension SearchViewController: UICollectionViewDataSourcePrefetching {
     
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+
+        for indexPath in indexPaths {
+            if !isEnd && list.count-2 == indexPath.item {
+                page += 1
+                callRequest(page: page)
+            }
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        print(#function)
+    }
 }
 
 extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
